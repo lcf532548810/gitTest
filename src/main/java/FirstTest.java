d2363aa0a7df56059cca0e060b7fd89c0ff0fea9
lcf532548810 <39626859+lcf532548810@users.noreply.github.com>
2018-09-25 18:40:30 +0800

    Update FirstTest.java

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -3,5 +3,8 @@ public class FirstTest {
     public static void main(String[] args) {
         System.out.println("first time");
     }
-
+    public void test(){
+        System.out.println("test update");
+    }
+    
 }
