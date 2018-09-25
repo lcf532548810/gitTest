b50c884af3a3c330df5594af403d31f956028e15
Fcl <532548810@qq.com>
2018-09-25 19:09:15 +0800

    second commit

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -3,6 +3,7 @@ public class FirstTest {
     public static void main(String[] args) {
         System.out.println("first time");
     }
+
     public void test(){
         System.out.println("test update");
     }
